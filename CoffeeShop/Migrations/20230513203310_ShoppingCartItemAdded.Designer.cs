593f7bfb45c72b95c96ac729177edec041caf089
Kelsey Crompton <kelsey.crompton@gmail.com>
2023-05-13 17:17:10 -0400

    Created ShoppingCartItem class, added session management and shopping cart methods

--- CoffeeShop/Migrations/20230513203310_ShoppingCartItemAdded.Designer.cs
+++ CoffeeShop/Migrations/20230513203310_ShoppingCartItemAdded.Designer.cs
@@ -3,6 +3,7 @@ using CoffeeShop.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CoffeeShop.Migrations
 {
     [DbContext(typeof(CoffeeShopDbContext))]
-    partial class CoffeeShopDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230513203310_ShoppingCartItemAdded")]
+    partial class ShoppingCartItemAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
